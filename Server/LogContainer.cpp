33ac5396a5118fec501a9ca965da114ab45d8c76
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-11-16 22:09:29 +0300

    Update LogContainer.cpp

--- Server/LogContainer.cpp
+++ Server/LogContainer.cpp
@@ -1,5 +1,6 @@
 #include "LogContainer.h"
-#include "ctime"
+#include <ctime>
+#include <iostream>
 
 std::list<Log>::iterator Find(std::list<Log>*, const char*);
 
@@ -13,17 +14,13 @@ int LogContainer::Update(const char* path, uint32_t period)
 
 	Paste->UpdateTime = period + static_cast<uint32_t>(time(nullptr));
 	Paste->Period = period;
-	for (auto it = Container.begin(); it != Container.end(); ++it)
-		if (it->UpdateTime > Paste->UpdateTime)
-		{
-			std::swap(*Paste, *it);
-			return 0;
-		}
 	return 0;
 }
 
 int LogContainer::Register(const char* path)
 {
+	if ((!Container.empty()) && (Find(&Container, path) != Container.end()))
+		return ERROR_EXIST_LOG;
 	Container.push_back({std::string(path), (uint32_t)(~0), (uint32_t)(~0)});
 	return 0;
 }
@@ -36,6 +33,20 @@ int LogContainer::Unregister(const char* path)
 	if(it == Container.end())
 		return ERROR_NO_LOG;
 	Container.erase(it);
+	return 0;
+}
+
+int LogContainer::SomeAction()
+{
+	uint32_t clock = static_cast<uint32_t>(time(nullptr));
+	if (Container.empty())
+		return 0;
+	for(auto it = Container.begin(); it != Container.cend(); ++it)
+		if(clock > it->UpdateTime)
+		{
+			std::cout << "path: " << it->Path << "\ntime: " << it->UpdateTime << '\n';
+			it->UpdateTime += it->Period;
+		}
 	return 0;
 }
 
