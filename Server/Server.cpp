32d6bf2a763eb2cf3149c19a898fe887c739e146
ivanrudn0303 <ivanrudn0303@gmail.com>
2018-11-22 20:16:29 +0300

    Update Server.cpp

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -4,6 +4,7 @@
 
 #define ERROR_BUF_OVERFLOW 2
 #define ERROR_WRONG_COMMAND 3
+#define ERROR_REQUEST 5
 
 #define BUF_SIZE 2048
 
@@ -11,7 +12,10 @@
 void Respond(SOCKET, LogContainer*, std::mutex*, std::stack<std::list<std::thread>::iterator>*, std::list<std::thread>::iterator);
 void PeriodicFunction(LogContainer*);
 int Handler(SOCKET, LogContainer*, char*, int);
-char* SplitStr(char*);
+
+#define COMMAND(request) int RESPOND_##request(SOCKET, LogContainer*, char*, int);
+#include "RequestCommand.h"
+#undef COMMAND
 
 
 int Server::FreeThreadResourses()
@@ -36,50 +40,50 @@ int Server::Start()
 	WORD wVersion;
 	WSAData WsaData;
 	wVersion = MAKEWORD(2, 0);
-	int err = WSAStartup(wVersion, &WsaData);
-	if (err != 0)
-		return err;
-	Socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (Socket == INVALID_SOCKET)
-	{
-		WSACleanup();
-		return INVALID_SOCKET;
-	}
+int err = WSAStartup(wVersion, &WsaData);
+if (err != 0)
+return err;
+Socket = socket(AF_INET, SOCK_STREAM, 0);
+if (Socket == INVALID_SOCKET)
+{
+	WSACleanup();
+	return INVALID_SOCKET;
+}
 
-	SOCKADDR_IN addr;
-	addr.sin_addr.S_un.S_addr = INADDR_ANY;
-	addr.sin_port = htons(Port);
-	addr.sin_family = AF_INET;
+SOCKADDR_IN addr;
+addr.sin_addr.S_un.S_addr = INADDR_ANY;
+addr.sin_port = htons(Port);
+addr.sin_family = AF_INET;
 
-	err = bind(Socket, (SOCKADDR *)&addr, sizeof(addr));
-	if (err == SOCKET_ERROR)
-	{
-		closesocket(Socket);
-		WSACleanup();
-		return SOCKET_ERROR;
-	}
-
-	err = listen(Socket, SOMAXCONN);
-	if (err == SOCKET_ERROR)
-	{
-		closesocket(Socket);
-		WSACleanup();
-		return SOCKET_ERROR;
-	}
-
-	std::thread PeriodicUpdater(PeriodicFunction, &Container);
-
-	while (true)
-	{
-		SOCKET NewSocket = accept(Socket, nullptr, nullptr);
-		ThreadList.push_front(std::thread());
-		ThreadList.front() = std::thread(Respond, NewSocket, &Container, &StackMutex, &FinishedThead, ThreadList.begin());
-		FreeThreadResourses();
-	}
-
-	PeriodicUpdater.join();
+err = bind(Socket, (SOCKADDR *)&addr, sizeof(addr));
+if (err == SOCKET_ERROR)
+{
 	closesocket(Socket);
 	WSACleanup();
+	return SOCKET_ERROR;
+}
+
+err = listen(Socket, SOMAXCONN);
+if (err == SOCKET_ERROR)
+{
+	closesocket(Socket);
+	WSACleanup();
+	return SOCKET_ERROR;
+}
+
+std::thread PeriodicUpdater(PeriodicFunction, &Container);
+
+while (true)
+{
+	SOCKET NewSocket = accept(Socket, nullptr, nullptr);
+	ThreadList.push_front(std::thread());
+	ThreadList.front() = std::thread(Respond, NewSocket, &Container, &StackMutex, &FinishedThead, ThreadList.begin());
+	FreeThreadResourses();
+}
+
+PeriodicUpdater.join();
+closesocket(Socket);
+WSACleanup();
 }
 
 Server::~Server()
@@ -87,15 +91,11 @@ Server::~Server()
 
 void Respond(SOCKET sock, LogContainer* base, std::mutex* mute, std::stack<std::list<std::thread>::iterator>* stack, std::list<std::thread>::iterator it)
 {
-	char buf[BUF_SIZE] = {0};
+	char buf[BUF_SIZE] = { 0 };
 	int len = recv(sock, buf, BUF_SIZE, 0);
-	while ((len > 0) && (len != SOCKET_ERROR))
-	{
-		base->Lock();
-		Handler(sock, base, buf, len);
-		base->Unlock();
-		len = recv(sock, buf, 2048, 0);
-	}
+//	base->Lock();
+	Handler(sock, base, buf, len);
+//	base->Unlock();
 	closesocket(sock);
 	mute->lock();
 	stack->push(it);
@@ -106,109 +106,86 @@ void PeriodicFunction(LogContainer* base)
 {
 	while (true)
 	{
-		base->Lock();
+//		base->Lock();
 		base->SomeAction();
-		base->Unlock();
+//		base->Unlock();
 		Sleep(450);
 	}
 }
 
-
-
-#define WRONG_COM() {\
-send(sock, "400 Bad Request\r\n\r\n", 19, 0);\
-return ERROR_WRONG_COMMAND;\
+#define BAD {\
+send(sock, "HTTP/1.1 409 Bad Request\r\n\r\n", 28, 0);\
+return ERROR_REQUEST;\
 }
 
-#define ANS_REG(num) {\
-	int err = num;\
-	if(err == 0)\
-		send(sock, "201 Created\r\n\r\n", 15, 0);\
-	else\
-		send(sock, "409 Conflict\r\n\r\n", 16, 0);\
-	buf = strstr(end + 1, "\r\n\r\n") + 4;\
-}
+#define COMMAND(request) if((start = strstr(buf, #request " ")) != nullptr)\
+	return RESPOND_##request(sock, data, start, length);\
+	else
 
-
-#define ANS_UNREG(num) {\
-	int err = num;\
-	if(err == 0)\
-		send(sock, "200 OK\r\n\r\n", 10, 0);\
-	else\
-		send(sock, "404 Not Found\r\n\r\n", 17, 0);\
-	buf = strstr(end + 1, "\r\n\r\n") + 4;\
-}
-
-#define ANS_SET(num) {\
-int err = num;\
-	if(err == 0)\
-		send(sock, "200 OK\r\n\r\n", 10, 0);\
-	else\
-		send(sock, "404 Not Found\r\n\r\n", 17, 0);\
-	buf = strstr(end + 1, "\r\n\r\n") + 4;\
-}
-
-int Handler(SOCKET sock, LogContainer *base, char *buf, int len)
+int Handler(SOCKET sock, LogContainer *data, char *buf, int length)
 {
-	if(len == BUF_SIZE)
-		return ERROR_BUF_OVERFLOW;
-	buf[len] = '\0';
-	printf("length: %d\n%s\n", len, buf);
-	while ((buf != nullptr) && (*buf != '\0'))
-	{
+	buf[length] = '\0';
+	std::cout << buf;
+	char *start = nullptr;
+#include "RequestCommand.h"
+	BAD
 
-		if (strncmp(buf, "REGISTER", 8) == 0)
-		{
-			char *end = SplitStr(buf);
-			if (end == nullptr)
-				WRONG_COM();
+	//	send(sock, "HTTP/1.1 200 OK\r\n\r\nhello\r\n\r\n", 28, 0);
+}
+#undef COMMAND
 
-			buf = strchr(buf, ' ') + 1;
-			*end = '\0';
-			ANS_REG(base->Register(buf));
-		}
-		else if (strncmp(buf, "UNREGISTER", 10) == 0)
-		{
-			char *end = SplitStr(buf);
-			if (end == nullptr)
-				WRONG_COM();
 
-			buf = strchr(buf, ' ') + 1;
-			*end = '\0';
-			ANS_UNREG(base->Unregister(buf));
-		}
-		else if (strncmp(buf, "SET", 3) == 0)
-		{
-			char *end = strchr(buf, '?');
-			if (end == nullptr)
-				WRONG_COM();
 
-			buf = strchr(buf, ' ') + 1;
-			*end = '\0';
-			char* num = strstr(end + 1, "UpdatePeriod=");
-			if (num == nullptr)
-				WRONG_COM();
-			num += 13;
-			uint32_t Period = atoi(num);
-			if(!Period)
-				WRONG_COM();
-			ANS_SET(base->Update(buf, Period));
-		}
-		else
-			WRONG_COM();
-	}
+int RESPOND_PUT(SOCKET sock, LogContainer* data, char* start, int length)
+{
+	if ((start = strstr(start, "/Log/")) == nullptr)
+		BAD
+		start += sizeof("/Log/") - 1;
+	uint32_t id = static_cast<uint32_t>(atoi(start));
+	if (data->ForceFlush(id))
+		BAD
+	send(sock, "HTTP/1.1 200 OK\r\n\r\n", 19, 0);
 	return 0;
 }
-#undef ANS_REG
-#undef ANS_UNREG
-#undef WRONG_COM
 
-char* SplitStr(char* str)
+int RESPOND_POST(SOCKET sock, LogContainer* data, char* start, int length)
 {
-	if (str == nullptr)
-		return nullptr;
-	str = strchr(str, ' ');
-	if (str == nullptr)
-		return nullptr;
-	return strchr(str + 1, ' ');
+	if((start = strstr(start, "/Log/")) == nullptr)
+		BAD
+	start += sizeof("/Log/") - 1;
+	uint32_t id = static_cast<uint32_t>(atoi(start));
+	
+	char* Update = strstr(start, "UpdatePeriod=");
+	if (Update == nullptr)
+		BAD
+	Update += sizeof("UpdatePeriod=") - 1;
+	uint32_t UpdatePeriod = static_cast<uint32_t>(atoi(Update));
+
+	char* path = strstr(start, "Path=");
+	if(path == nullptr)
+		BAD
+	path += sizeof("Path=") - 1;
+	char key[] = " \n\r\0";
+	char *end = strpbrk(path, key);
+	if (end == nullptr)
+		BAD
+	*end = '\0';
+
+	if (data->Register(path, id, UpdatePeriod))
+		BAD
+	send(sock, "HTTP/1.1 201 Created\r\n\r\n", 24, 0);
+	return 0;
 }
+
+int RESPOND_DELETE(SOCKET sock, LogContainer* data, char* start, int length)
+{
+	if ((start = strstr(start, "/Log/")) == nullptr)
+		BAD
+		start += sizeof("/Log/") - 1;
+	uint32_t id = static_cast<uint32_t>(atoi(start));
+	if (data->Unregister(id))
+		BAD
+	send(sock, "HTTP/1.1 200 OK\r\n\r\n", 19, 0);
+	return 0;
+}
+#undef BAD
