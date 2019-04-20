3f6f75d6a593e14a1d895631bdee72e4ae54fbc7
Ivan Kozlov <ivanrudn0303@Ivans-iMac.local>
2019-04-20 09:46:29 -0700

    Swift: transform Task1 into Task2

--- Sem6/SwiftCource/Task1/HelloWorld/Functions.swift
+++ Sem6/SwiftCource/Task1/HelloWorld/Functions.swift
@@ -8,28 +8,57 @@
 
 import Foundation
 
-func EratBoundary(_ N:Int) -> Int
+class PrimeFinder
 {
-    var tmp = N
-    if(tmp < 6) {tmp = 6}
-    return tmp * Int(log(Double(tmp)) + 1) + tmp * Int(log(log(Double(tmp))) + 1)
-}
-
-func EratPrime(_ N:Int) -> Int
-{
-    if (N < 1) {return -1}
-    let size = EratBoundary(N)
-    var grid = Array(repeating: true, count: size)
-    var counter = 0
-    var start = 2
-    while (counter != N)
+    var lock = NSLock()
+    var stopped = true
+    var sem = DispatchSemaphore(value: 0)
+    var grid = [Bool]()
+    var gridReady = false
+    
+    func EratBoundary(_ N:Int?) -> Int
     {
-        if (grid[start])
-        {
-            counter += 1
-            for i in stride(from: start * start, to: size, by: start) {grid[i] = false}
-        }
-        start += 1
+        guard var tmp = N else {return -1}
+        if(tmp < 6) {tmp = 6}
+        return tmp * Int(log(Double(tmp)) + 1) + tmp * Int(log(log(Double(tmp))) + 1)
     }
-    return start - 1
+
+    func EratPrime(_ n:Int?) -> Int
+    {
+        guard let N = n else {return -1}
+        if (N < 1) {return -1}
+        let size = EratBoundary(N)
+        if ((size <= grid.count) && gridReady) {return Find(N)}
+        gridReady = false
+        grid = Array(repeating: true, count: size)
+        var counter = 0
+        var start = 2
+        while (counter != N)
+        {
+            if (grid[start])
+            {
+                counter += 1
+                for i in stride(from: start * start, to: size, by: start) {
+                    grid[i] = false
+                    if(stopped) {return -1}
+                }
+                if(stopped) {return -1}
+            }
+            start += 1
+        }
+        gridReady = true
+        return start - 1
+    }
+    func Find(_ n: Int) -> Int
+    {
+        var num = n
+        var start = 2
+        while (num > 0)
+        {
+            if(grid[start]) {num -= 1}
+            start += 1
+            if(stopped) {return -1}
+        }
+        return start - 1
+    }
 }
--- Sem6/SwiftCource/Task1/HelloWorld/ViewController.swift
+++ Sem6/SwiftCource/Task1/HelloWorld/ViewController.swift
@@ -14,17 +14,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var Output: UILabel!
     @IBOutlet weak var Input: UITextField!
     @IBOutlet weak var Butt: UIButton!
-
+    
+    var finder = PrimeFinder()
+    
     @IBAction func Interrupt(_ sender: Any) {
-        if let input = self.Input.text
-        {
-            self.Output.text = "Answer \(EratPrime(Int(input)!))"
+        finder.lock.lock()
+        if(!finder.stopped) {
+            finder.stopped = true
+            finder.lock.unlock()
+            finder.sem.wait()
+            finder.stopped = false
+        } else{
+            finder.stopped = false
+            finder.lock.unlock()
         }
+        guard let input = self.Input.text else {return}
+        
+        let queue = DispatchQueue.global(qos: .userInitiated)
+        queue.async(execute: {[weak this = self] in
+                guard let that = this else{return}
+                let res = that.finder.EratPrime(Int(input))
+                var resStr = ""
+                that.finder.lock.lock()
+                if that.finder.stopped{
+                    resStr = String("Stopped")
+                    that.finder.sem.signal()
+                } else{
+                    resStr = String("Answer \(res)")
+                    that.finder.stopped = true
+                }
+                that.finder.lock.unlock()
+                DispatchQueue.main.sync {
+                    that.Output.text = resStr
+                }
+            })
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
